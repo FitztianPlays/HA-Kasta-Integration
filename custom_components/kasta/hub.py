c7ab28b6ee5dff5a5514bdf857f2e60737e69677
Christian F <christianfdev@gmail.com>
2023-07-03 22:36:42 +1000

    Add entity polling

--- custom_components/kasta/hub.py
+++ custom_components/kasta/hub.py
@@ -1,5 +1,10 @@
+from datetime import timedelta
 import requests
 
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+import logging
+_LOGGER = logging.getLogger(__name__)
 
 class BasicHub:
     def __init__(self, host: str) -> None:
@@ -19,3 +24,19 @@ class BasicHub:
     def toggle(self, name):
         r = requests.get(f"http://{self.host}/toggle?name=" + name, timeout=10000)
         return r.json()
+
+
+class MyCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass, my_api) -> None:
+        self.hass = hass
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Astra Polling",
+            update_interval=timedelta(seconds=3),
+        )
+        self.my_api = my_api
+
+    async def _async_update_data(self):
+        stat = await self.hass.async_add_executor_job(self.my_api.get_devices)
+        return stat
